ea00afc36b72a8d7ca69f53006d6c090f4e58c9a
pjs990301 <p990301@gmail.com>
2022-07-28 18:18:54 +0900

    admin 페이지 수정

--- server/myapp/admin.py
+++ server/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from myapp.models import Users
+from .models import Users, Activity
 
 admin.site.register(Users)
+admin.site.register(Activity)
