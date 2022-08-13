2f19101efb15a8062701af6be962c3d2ffe53c9b
pjs990301 <p990301@gmail.com>
2022-08-13 14:13:14 +0900

    move com

--- pi_request.py
+++ pi_request.py
@@ -2,20 +2,23 @@ import requests
 import subprocess
 import json
 
-
 serial = subprocess.check_output('cat /proc/cpuinfo | grep Serial | awk \'{print $3}\'', shell=True)
 
-iwconfig = subprocess.check_output('iwconfig wlan0',shell=True)
+iwconfig = subprocess.check_output('iwconfig wlan0', shell=True)
 bssid = ""
+# Python3 Version
+# index = iwconfig.decode.find("Access Point: ")
+
+# Python2 Version
 index = iwconfig.find("Access Point: ")
-for i in range(index + 14,index + 31): 
-    bssid +=iwconfig[i]
+for i in range(index + 14, index + 31):
+    bssid += iwconfig[i]
 
 url = "http://blue-sun.kro.kr:9000/pi/"
 data = {
-	"serial_number" : serial, 
-	"mac_address" : bssid, 
-	"type" : 1
-	}
+    "serial_number": serial,
+    "mac_address": bssid,
+    "type": 1
+}
 headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 requests.post(url, data=json.dumps(data), headers=headers)
